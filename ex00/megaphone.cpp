4726fff5bb321a10f1e713126e3462a1f3221328
naharagu <76852123+naharagu@users.noreply.github.com>
2023-03-04 14:45:20 +0100

    first

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,22 +1,20 @@
 #include <iostream>
 #include <cctype>
 #include <cstring>
-using std::cout;
-using std::endl;
 
 int main(int argc, char **argv)
 {
 	if (argc == 1)
 	{
-		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
 
 	for (int i = 1; i < argc; i++)
 	{
 		for (size_t j = 0; j < strlen(argv[i]); j++)
-			cout << (char)toupper(argv[i][j]);
+			std::cout << (char)toupper(argv[i][j]);
 	}
-	cout << endl;
+	std::cout << std::endl;
 	return (0);
 }
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <cctype>
 #include <cstring>
-using std::cout;
-using std::endl;
 
 int main()
 {
+	
 	return (0);
 }
